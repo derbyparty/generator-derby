70393e38de866b4365b63ae9eb3e608bf4639f84
Artur Zayats <zag2art@gmail.com>
2014-06-24 19:38:43 +0400

    fix coffescript, clear html and css, change derby version

--- component/templates/src/_component.coffee
+++ component/templates/src/_component.coffee
@@ -1,9 +1,9 @@
 path = require "path"
 
 module.exports = class <%= className %>
-  view = path.join __dirname, "<%= component %>"
-  style = path.join __dirname, "<%= component %>"
+  view: path.join __dirname, "<%= component %>"
+  style: path.join __dirname, "<%= component %>"
 
-  init = (model) =>
+  init: (model) ->
 
-  create = (model, dom) =>
+  create: (model, dom) ->
--- component/templates/src/_component.js
+++ component/templates/src/_component.js
@@ -2,11 +2,15 @@ var path = require("path");
 
 module.exports = <%= className %>;
 
-function <%= className %>() {}
+function <%= className %>() {};
 
 <%= className %>.prototype.view = path.join(__dirname, "<%= component %>");
 <%= className %>.prototype.style = path.join(__dirname, "<%= component %>");
 
-<%= className %>.prototype.init = function(model) {};
+<%= className %>.prototype.init = function(model){
 
-<%= className %>.prototype.create = function(model, dom) {};
+};
+
+<%= className %>.prototype.create = function(model, dom){
+
+};
